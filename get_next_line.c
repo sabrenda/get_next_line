b3bab75e6f3b4fbb082044d52693761f5f34a9d0
sabrenda <nob_32@mail.ru>
2020-11-25 23:18:31 +0300

    lalal

---
+++ get_next_line.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabrenda <sabrenda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/25 18:24:46 by sabrenda          #+#    #+#             */
+/*   Updated: 2020/11/25 23:04:17 by sabrenda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+
+int		get_next_line(int fd, char **line)
+{
+	unsigned int	n;
+	unsigned int	last;
+	char	*buffer;
+
+	n = 0;
+	m = 0;
+	while(1)
+	{
+	if (!(buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char))))
+		return (-1);
+	last = read(fd, buffer, BUFFER_SIZE);
+	buffer[last] = '\0';
+	}
+
+}
+int		read_from_fd(int fd)
+{
+	char	*buf;
+	int		rs;
+
+	rs = 0;
+	if (!(buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char))))
+		return (-1);
+	rs = read(fd, buf, BUFFER_SIZE);
+	buf[rs] = '\0';
+	return (0);
+}
+
+int main()
+{
+	int fd = open("file", O_RDONLY);
+	char *line;
+	int i;
+	while ((i = get_next_line(fd, &line)))
+	{
+		printf("i = %d %s\n", i, line);
+		free(line);
+	}
+	printf("i = %d %s\n", i, line);
+	free(line);
+	return (0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,18 @@
 /*   By: sabrenda <sabrenda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 17:45:03 by sabrenda          #+#    #+#             */
-/*   Updated: 2020/11/25 17:47:29 by sabrenda         ###   ########.fr       */
+/*   Updated: 2020/11/25 21:45:48 by sabrenda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE.h
-# define GET_NEXT_LINE.h
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <unistd.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 
+int		get_next_line(int fd, char **line);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
