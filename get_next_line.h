f222f099054637d0e414541a6ca14b2f41e95418
Akhmed A <nob_32@mail.ru>
2020-12-05 15:03:30 +0300

    end

--- get_next_line.h
+++ get_next_line.h
@@ -3,40 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Ahmed <Ahmed@student.42.fr>                +#+  +:+       +#+        */
+/*   By: sabrenda <sabrenda@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/25 17:45:03 by sabrenda          #+#    #+#             */
-/*   Updated: 2020/12/05 13:56:02 by Ahmed            ###   ########.fr       */
+/*   Created: 2020/12/05 14:57:53 by sabrenda          #+#    #+#             */
+/*   Updated: 2020/12/05 15:00:04 by sabrenda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE	1024
-# endif
 
 # include <unistd.h>
 # include <stdlib.h>
-# include <stdio.h>
 # include <fcntl.h>
+
+int			get_next_line(int fd, char **line);
+char		*ft_strjoin(char *s1, char *s2);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strdup(const char *str);
+char		*ft_rasengan(char	**s_line);
+char		*ft_chidori(char *point_n);
+int			ft_get_line(int fd, char **line, char **s_line);
+void		ft_bzero(char *s, size_t n);
+t_gnl		*new_list(int fd);
+size_t		ft_strlen(const char *str);
 
 typedef struct		s_gnl
 {
-	int 			fd;
+	int				fd;
 	char			*s_line;
 	struct s_gnl	*next;
 }					t_gnl;
-
-int		get_next_line(int fd, char **line);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *str);
-char	*ft_rasengan(char	**s_line);
-char	*ft_chidori(char *point_n);
-int		ft_get_line(int fd, char **line, char **s_line);
-void	ft_bzero(char *s, size_t n);
-t_gnl	*new_list(int fd);
-size_t	ft_strlen(const char *str);
 
 #endif
